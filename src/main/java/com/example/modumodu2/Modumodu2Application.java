61b1fa38fc4b0925d74f351e3c40a2bb4d1fbd97
전승원 <jswa7308@naver.com>
2022-03-08 23:04:50 +0900

    인텔리제이에서 처ì음으로 remote 한 결과물

---
+++ src/main/java/com/example/modumodu2/Modumodu2Application.java
@@ -0,0 +1,13 @@
+package com.example.modumodu2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Modumodu2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Modumodu2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/modumodu2/Modumodu2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modumodu2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Modumodu2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
